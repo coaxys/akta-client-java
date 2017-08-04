a4e38e05efd92df2a42842d3ec740ba9a68d33da
Nicolas Giard <nicolas.giard@coaxys.com>
2017-08-04 17:27:51 +0200

    feat: l'api retourne désormais plus d'urls

--- src/main/java/com/coaxys/akta/AktaFile.java
+++ src/main/java/com/coaxys/akta/AktaFile.java
@@ -1,13 +1,11 @@
 package com.coaxys.akta;
 
-import java.util.List;
-
 public class AktaFile {
 
     public String uid;
     public String project;
     public String arbo;
     public String fileName;
-    public List<AktaUrl> urls;
+    public AktaUrl urls;
 
 }
